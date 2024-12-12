14e5f5f4fb9c9f3fdfe29b9a850a6b4e648eb977
xiaoSB33 <caizhuch1199w@qq.com>
2024-12-12 11:47:10 +0800

    更新 jdknsn.lua

--- jdknsn.lua
+++ jdknsn.lua
@@ -244,7 +244,7 @@ Char:MoveTo(box.Position + Vector3.new(0,.5,0))
 Tab:AddButton({
 	Name = "飞行脚本可隐藏",
 	Callback = function()
-     loadstring(game:HttpGet("https://github.com/xiaoSB33/M416/raw/main/flight.lua", true))()
+          loadstring(game:HttpGet('https://pastebin.com/raw/AjDmq385'))()
   	end    
 })
 
