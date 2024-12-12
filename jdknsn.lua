6856cdfcb9e2c7e1dc1c2457093fa6565ccff395
xiaoSB33 <caizhuch1199w@qq.com>
2024-12-12 11:32:00 +0800

    更新 jdknsn.lua

--- jdknsn.lua
+++ jdknsn.lua
@@ -1,5 +1,5 @@
 local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/LOL/main/Orion.lua", true))()
- local Window = OrionLib:MakeWindow({Name = "心脚本V1.3.0", HidePremium = false, SaveConfig = false, ConfigFolder = "rbxassetid://6026568198"}) 
+ local Window = 心脚本:MakeWindow({Name = "心脚本V1.3.0", HidePremium = false, SaveConfig = false, ConfigFolder = "rbxassetid://6026568198"}) 
   
  OrionLib:MakeNotification({ 
  Name = "心脚本", 
