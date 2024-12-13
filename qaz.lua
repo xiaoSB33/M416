5518d9759b3bc77d7525f9974a71c7da3d709653
CanLun33 <caizhuch1199w@qq.com>
2024-12-13 12:58:34 +0800

    更新

---
+++ qaz.lua
@@ -0,0 +1,145 @@
+local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))() 
+ local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))() 
+
+ wait(1) 
+ Notification:Notify( 
+     {Title = "白灰", Description = "白灰正在加载"}, 
+     {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "image"}, 
+     {Image = "http://www.roblox.com/asset/?id=4483345998", ImageColor = Color3.fromRGB(255, 84, 84)} 
+ ) 
+ wait(2) 
+ Notification:Notify( 
+     {Title = "白灰", Description = "准备好了！"}, 
+     {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "image"}, 
+     {Image = "http://www.roblox.com/asset/?id=4483345998", ImageColor = Color3.fromRGB(255, 84, 84)} 
+ )
+ wait(0.9)
+local Cloudlib = loadstring(game:HttpGet("https://gist.githubusercontent.com/XiaoYunCN/c212174d6263d5a43e95a59110a2724b/raw/c89c61fa5c8149a2554b66f618a32cd3e4d42877/gistfile1.txt", true))()
+
+local window = Cloudlib:new("白灰--自然灾害生存")
+
+local creds = window:Tab("主要功能",'4483345998')
+
+local Tab = creds:section("主要的",true)
+
+Tab:Label("主要的")
+
+Tab:Toggle("自动农场胜出", "ToggleInfo", false, function(bool)
+    _G.autowinfarm = bool;
+    while wait(.1) do
+        if _G.autowinfarm == true then
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-236, 180, 360, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        end
+end
+end)
+
+Tab:Toggle("地图投票用户界面", "Map Voting UI", false, function(bool)
+if bool == false then do game.Players.LocalPlayer.PlayerGui.MainGui.MapVotePage.Visible = false
+    end
+end
+if bool == true then do game.Players.LocalPlayer.PlayerGui.MainGui.MapVotePage.Visible = true
+    end
+end
+end)
+
+Tab:Toggle("在水上行走", "ToggleInfo", false, function(bool)
+ if bool == false then do game.Workspace.WaterLevel.CanCollide = false
+                            game.Workspace.WaterLevel.Size = Vector3.new(10, 1, 10)
+                        end
+                    end
+                    if bool == true then do game.Workspace.WaterLevel.CanCollide = true
+                            game.Workspace.WaterLevel.Size = Vector3.new(5000, 1, 5000)
+                        end
+                    end
+end)
+
+Tab:Toggle("游戏岛悬崖碰撞", "Togglelnfo", false, function(bool)
+for i, v in pairs (game.workspace:GetDescendants())do
+                                if v.Name == 'LowerRocks' then
+                                    v.CanCollide = bool
+                                end
+                            end
+end)
+
+Tab:Button("禁用坠落损坏",function()
+local FallDamageScript = (game.Players.LocalPlayer.Character ~= nil) and game.Players.LocalPlayer.Character:FindFirstChild("FallDamageScript") or nil
+                                if FallDamageScript then
+                                FallDamageScript:Destroy()
+                                end
+end)
+
+Tab:Toggle("自动禁用坠落伤害", "Toggleelnfo", false, function(bool)
+_G.NoFallDamage = bool;
+                            while wait(0.5) do
+                                    if _G.NoFallDamage == true then
+                            local FallDamageScript = (game.Players.LocalPlayer.Character ~= nil) and game.Players.LocalPlayer.Character:FindFirstChild("FallDamageScript") or nil
+                            if FallDamageScript then
+                            FallDamageScript:Destroy()
+                            end end end
+end)
+
+Tab:Button("打印下一次灾难(/console)",function()
+warn(game.Players.LocalPlayer.Character.SurvivalTag.Value)
+end)
+
+Tab:Button("移除灾难界面(暴风雪和沙尘暴)",function()
+game.Players.LocalPlayer.PlayerGui.BlizzardGui:destroy()
+		game.Players.LocalPlayer.PlayerGui.SandStormGui:destroy()
+end)
+
+local Tab5 = creds:section("主要的",true)
+
+Tab5:Label("地图")
+
+Tab5:Button("传送到地图",function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-115.828506, 65.4863434, 18.8461514, 0.00697017973, 0.0789371505, -0.996855199, -3.13589936e-07, 0.996879458, 0.0789390653, 0.999975681, -0.000549906865, 0.00694845384)
+end)
+
+Tab5:Button("游戏岛",function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-83.5, 38.5, -27.5, -1, 0, 0, 0, 1, 0, 0, 0, -1)
+end)
+
+Tab5:Button("产卵塔",function()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-280, 170, 341, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+end)
+local creds = window:Tab("玩家",'4483345998')
+
+local Tab3 = creds:section("玩家",true)
+
+Tab3:Slider('步行速度', 'SliderInfo', 16, 16, 250,false, function(value)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = value
+end)
+
+Tab3:Slider('跳跃能力', 'SliderInfo', 16, 16, 250,false, function(value)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = value
+end)
+
+local Tab4 = creds:section("通用",true)
+
+Tab4:Button("单击TP工具",function()
+mouse = game.Players.LocalPlayer:GetMouse()
+                tool = Instance.new("Tool")
+                tool.RequiresHandle = false
+                tool.Name = "Click Teleport"
+                tool.Activated:connect(function()
+                local pos = mouse.Hit+Vector3.new(0,2.5,0)
+                pos = CFrame.new(pos.X,pos.Y,pos.Z)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = pos
+                end)
+                tool.Parent = game.Players.LocalPlayer.Backpack
+end)
+
+Tab4:Button("反AFK",function()
+wait(0.5)
+    local bb=game:service'VirtualUser'
+    game:service'Players'.LocalPlayer.Idled:connect(function()
+    bb:CaptureController()
+    bb:ClickButton2(Vector2.new())
+    end)
+ 
+    print("Antiafk enabled")
+end)
+
+Tab4:Button("飞行",function()
+loadstring(game:HttpGet("https://pastebin.com/raw/gqv7PXAa"))()
+end)
