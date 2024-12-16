17a8b0fc4d5c501169112a1ed9aeea710517928d
CaiLun33 <caizhuch1199w@qq.com>
2024-12-16 19:39:29 +0800

    更新 flight.lua

--- flight.lua
+++ flight.lua
@@ -1,6 +1,6 @@
 game:GetService("StarterGui"):SetCore("SendNotification",{
                 Title = "xiaoSB33";
-                Text ="QQ：3126295205";
+                Text ="QQ：乐子";
                 Duration = 2.5;
             })
 -- Gui to Lua
@@ -64,7 +64,7 @@ TextLabel.BackgroundColor3 = Color3.fromRGB(242, 60, 255)
 TextLabel.Position = UDim2.new(0.469327301, 0, 0, 0)
 TextLabel.Size = UDim2.new(0, 100, 0, 28)
 TextLabel.Font = Enum.Font.SourceSans
-TextLabel.Text = "彩云脚本飞行"
+TextLabel.Text = "云中心脚本飞行"
 TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
 TextLabel.TextScaled = true
 TextLabel.TextSize = 14.000
