204ef2b5a482ef185eccc6b182a32669f32bb0bc
CaiLun33 <caizhuch1199w@qq.com>
2024-12-16 19:38:15 +0800

    更新 jdknsn.lua

--- jdknsn.lua
+++ jdknsn.lua
@@ -2,8 +2,8 @@ local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Xiao
  local Window =   OrionLib:MakeWindow({Name = "云脚本V7.0", HidePremium = false, SaveConfig = false, ConfigFolder = "rbxassetid://6026568198"}) 
   
  OrionLib:MakeNotification({ 
- Name = "云脚本", 
- Content = "欢迎来到云脚本！", 
+ Name = "云中心脚本", 
+ Content = "欢迎来到云中心脚本！", 
  Image = "rbxassetid://4483345998", 
  Time = 5 
  })
