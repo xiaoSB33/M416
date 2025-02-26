4c41043509a92839aa08804db42f6219b792ed3d
CaiLun33 <caizhuch1199w@qq.com>
2025-02-26 19:11:38 +0800

    更新 jdknsn.lua

--- jdknsn.lua
+++ jdknsn.lua
@@ -1,2 +1,2 @@
-setclipboard("640304618")
-game:GetService("Players").LocalPlayer:Kick("脚本已更新，请进入脚本中心获取脚本，群号似乎已复制到你的剪贴板640304618")
+setclipboard("fw")
+game:GetService("Players").LocalPlayer:Kick("不好用是吧，c你妈")
