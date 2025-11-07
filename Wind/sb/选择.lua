f12584ea092fb5f6a7416c7c1f5fce33b9d8e443
CaiLun33 <caizhuch1199w@qq.com>
2025-11-07 22:17:26 +0800

    更新 选择.lua

--- Wind/sb/选择.lua
+++ Wind/sb/选择.lua
@@ -1,5 +1,5 @@
 return {
-    ["自然灾害"] = "https://github.com/XiaoYunUwU/",
+    ["自然灾害"] = "https://github.com/xiaoSB33/M416/raw/main/Wind%2Fsb",
     ["监狱人生"] = "https://github.com/xiaoSB33/M416/raw/main/Wind%2Fsb%2F%E7%9B%91%E7%8B%B1%E4%BA%BA%E7%94%9F.LUA",
     [""] = "https://github.com/XiaoYunUwU/"
 }
