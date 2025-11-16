ea016ee28e5b6bdd37be97fe9b121c2f87b594b5
CaiLun33 <caizhuch1199w@qq.com>
2025-11-16 16:27:38 +0800

    更新 选择.lua

--- Wind/sb/选择.lua
+++ Wind/sb/选择.lua
@@ -1,14 +1,14 @@
-
 return {
     ["飞车"] = "https://github.com/xiaoSB33/M416/raw/main/985.lua",
     ["无敌少侠"] = "https://raw.githubusercontent.com/ke9460394-dot/ugik/refs/heads/main/%E6%97%A0%E6%95%8C%E5%B0%91%E4%BE%A0%E9%A3%9E%E8%A1%8Cr6.txt",
     ["飞行"] = "https://github.com/xiaoSB33/M416/raw/main/fly.lua",
     ["自然灾害"] = "https://github.com/xiaoSB33/M416/raw/main/Wind%2Fsb%2F%E8%87%AA%E7%84%B6%E7%81%BE%E5%AE%B3.LUA",
     ["监狱人生"] = "https://github.com/xiaoSB33/M416/raw/main/Wind%2Fsb%2F%E7%9B%91%E7%8B%B1%E4%BA%BA%E7%94%9F.LUA",
+    ["墨水游戏"] = "https://github.com/xiaoSB33/M416/raw/main/Wind%2Fsb%2F%E5%A2%A8%E6%B0%B4%E6%B8%B8%E6%88%8F.LUA",
     ["力量传奇"] = "https://github.com/xiaoSB33/M416/raw/main/Wind%2Fsb%2F%E4%BC%A0%E5%A5%87.lua",
     ["跳跃点击器"] = "https://github.com/xiaoSB33/M416/raw/main/Wind%2Fsb%2F%E8%B7%B3%E8%B7%83%E7%82%B9%E5%87%BB%E5%99%A8.LUA",
     ["速度传奇"] = "https://github.com/xiaoSB33/M416/raw/main/Wind%2Fsb%2F%E9%80%9F%E5%BA%A6%E4%BC%A0%E5%A5%87.lua",
     ["忍者传奇"] = "https://github.com/xiaoSB33/M416/raw/main/Wind%2Fsb%2F%E5%BF%8D%E8%80%85%E4%BC%A0%E5%A5%87.lua",
-    ["ESP绘制（有bug）"] = "https://github.com/xiaoSB33/M416/raw/main/Wind%2Fsb%2FESP.lua",
-    ["自瞄（有bug）"] = "https://github.com/xiaoSB33/M416/raw/main/Wind%2Fsb%2F%E7%96%AB.lua",
+    ["ESP绘制"] = "https://github.com/xiaoSB33/M416/raw/main/Wind%2Fsb%2FESP.lua",
+    ["自描"] = "https://github.com/xiaoSB33/M416/raw/main/Wind%2Fsb%2F%E7%96%AB.lua",
 }
