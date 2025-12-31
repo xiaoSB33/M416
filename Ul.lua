638f84ee491417ec4dadedcd5711666781dd6b77
CaiLun33 <caizhuch1199w@qq.com>
2025-12-31 23:17:10 +0800

    更新 Ul.lua

--- Ul.lua
+++ Ul.lua
@@ -244,12 +244,13 @@ function vu1.new(pu27, p28, p29)
     v50.Image = "rbxassetid://118534129318519"
     v50.ImageColor3 = Color3.fromRGB(255, 255, 255)
     v50.SliceCenter = Rect.new(49, 49, 450, 450)
+       -- 创建背景图片
     local v59 = Instance.new("ImageLabel")
     v59.Name = "BackgroundImage"
     v59.Image = "rbxassetid://81503152709972"  -- 修正为正确的图片ID
     v59.Size = UDim2.new(1, 0, 1, 0)
     v59.Position = UDim2.new(0, 0, 0, 0)  -- 确保位置正确
-    v59.BackgroundTransparency = 10  -- 背景透明
+    v59.BackgroundTransparency = 1  -- 背景透明
     v59.ImageTransparency = 0  -- 图片不透明
     v59.ZIndex = 0  -- 调整为有效ZIndex
     v59.ScaleType = Enum.ScaleType.Crop  -- 添加缩放类型
